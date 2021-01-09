0d8996692edec7c0bd9c92ccfff4c8bf9dfa0130
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-01-09 18:21:16 +0530

    server testing

--- btmchatapp-backend/index.js
+++ btmchatapp-backend/index.js
@@ -1,1 +1,24 @@
-console.log("chat app backend")
+//import express
+const express = require("express")
+const app = express()
+
+//port number
+
+const port = 5000
+
+//first api response
+
+app.get("/test",(req,res)=>{
+    res.send("fuck")
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on the port ${port}`)
+})
+
+//db connection
+
+// mongodb+srv://admin:<password>@cluster0.wgjmp.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+//
+
